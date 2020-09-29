993b215602ff57801b66e7248710b9e59b7b8e5a
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-29 11:25:36 -0400

    updating signup as well

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 import { LOG_IN, SIGN_UP } from "./types.ts";
 
-const getToken = async () => {
+const getInstance = async () => {
   const response = await axios.get("/api/token");
-  return response.headers["x-csrf-token"];
+  const instance = axios.create({
+    baseURL: "/api",
+    headers: { "X-CSRF-Token": response.headers["x-csrf-token"] },
+  });
+  return instance
 };
 
 export const login = (values, history) => async (dispatch) => {
-  let token = await getToken();
-  console.log(token);
-  const response = await axios.post("/api/login", values, {
-    timeout: 1000,
-    headers: { "X-CSRF-Token": token },
-  });
+  let instance = await getInstance();
+  const response = instance.post("/login", values)  
   history.push("/");
   dispatch({
     type: LOG_IN,
@@ -21,7 +21,8 @@ export const login = (values, history) => async (dispatch) => {
 };
 
 export const signUp = (values, history) => async (dispatch) => {
-  const response = await axios.post("/api/signup", values);
+  let instance = await getInstance();
+  const response = await instance.post("/signup", values);
   history.push("/");
   dispatch({
     type: SIGN_UP,
