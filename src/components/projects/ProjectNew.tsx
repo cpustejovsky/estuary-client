4861fe0fc3849a4b27694b2c42ef804f2dcb4f04
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-30 12:27:09 -0400

    getting it working; adding todo to fix any types

--- src/components/projects/ProjectNew.tsx
+++ src/components/projects/ProjectNew.tsx
@@ -1,29 +1,62 @@
 import React from "react";
 import { Formik, Field } from "formik";
-import { connect } from "react-redux";
+import { connect, ConnectedProps } from "react-redux";
 import { Button, TextField } from "@material-ui/core";
 import DateFnsUtils from "@date-io/date-fns";
+import {Redirect} from "react-router-dom"
 import { createProject, updateProject } from "../../actions";
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from "@material-ui/pickers";
+import { User, AppState } from "../../models/"
+
 //TODO: potential memory leak here because I'm unmounting and not cleaning up. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function
-function ProjectNew({
-  id,
-  show,
-  note,
-  project,
-  edit,
-  deleteNote,
-  history,
+//TODO: replace all the 'any' types
+
+const mapState = (state: AppState) => ({
+  auth: state.auth,
+  user: state.user,
+  projects: Object.values(state.projects)
+})
+const mapDispatch = {
   createProject,
-  toggleProjectNew,
-  toggleActionable,
-  updateProject,
-  toggleEdit
-}) {
-  const submitValues = (values) => {
+  updateProject
+}
+
+const connector = connect(mapState, mapDispatch)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type Props = PropsFromRedux & {
+  show: boolean,
+  edit?: boolean,
+  note?: any,
+  project?: any,
+  id?: any,
+  deleteNote?: any,
+  toggleProjectNew?: any,
+  toggleActionable?: any,
+  toggleEdit?: any,
+}
+
+
+function ProjectNew(props: Props) {
+  const { 
+    id,
+    show,
+    note,
+    project,
+    edit,
+    deleteNote,
+    createProject,
+    toggleProjectNew,
+    toggleActionable,
+    updateProject,
+    toggleEdit } = props
+
+
+  const submitValues = (values: any) => {
     if (edit) {
       values.projectId = id;
       updateProject(values);
@@ -31,18 +64,19 @@ function ProjectNew({
       return;
     }
     if (note) {
-      createProject(values, history);
+      createProject(values);
       deleteNote(note.id);
       toggleProjectNew();
       toggleActionable();
       return;
     } else {
-      createProject(values, history);
-      history.push("/projects/list");
+      createProject(values);
+      return <Redirect to="/projects/list" />;
     }
   };
 
-  const DatePickerField = ({ field, handleBlur, form, ...other }) => {
+  const DatePickerField = (props: any) => {
+    const { field, handleBlur, form, ...other } = props
     const currentError = form.errors[field.name];
 
     return (
@@ -107,44 +141,44 @@ function ProjectNew({
             handleSubmit,
             isSubmitting,
           }) => (
-            <form onSubmit={handleSubmit}>
-              <div className="input-field center">
-                {project ? "" : <h1>New Project</h1>}
-                <div className="margin-top">
-                  <TextField
-                    label="Title"
-                    variant="outlined"
-                    name="title"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.title}
-                  />
-                </div>{" "}
-                <div className="margin-top">
-                  <TextField
-                    label="Description"
-                    variant="outlined"
-                    name="description"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.description}
-                  />
-                </div>{" "}
-                <div className="margin-top">
-                  <Field name="date" component={DatePickerField} />
-                </div>{" "}
-                <div className="margin-top">
-                  <Button
-                    onClick={() => {
-                      handleSubmit();
-                    }}
-                  >
-                    {project ? "Update Project" : "Create Project"}
-                  </Button>
+              <form onSubmit={handleSubmit}>
+                <div className="input-field center">
+                  {project ? "" : <h1>New Project</h1>}
+                  <div className="margin-top">
+                    <TextField
+                      label="Title"
+                      variant="outlined"
+                      name="title"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.title}
+                    />
+                  </div>{" "}
+                  <div className="margin-top">
+                    <TextField
+                      label="Description"
+                      variant="outlined"
+                      name="description"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.description}
+                    />
+                  </div>{" "}
+                  <div className="margin-top">
+                    <Field name="date" component={DatePickerField} />
+                  </div>{" "}
+                  <div className="margin-top">
+                    <Button
+                      onClick={() => {
+                        handleSubmit();
+                      }}
+                    >
+                      {project ? "Update Project" : "Create Project"}
+                    </Button>
+                  </div>
                 </div>
-              </div>
-            </form>
-          )}
+              </form>
+            )}
         </Formik>
       </div>
     );
@@ -153,4 +187,4 @@ function ProjectNew({
   }
 }
 
-export default connect(null, { createProject, updateProject })(ProjectNew);
+export default connector(ProjectNew);
