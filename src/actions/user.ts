631f093a402be9e48dbd7b2d76c33907733b60b5
cpustejovsky <charles.pustejovsky@gmail.com>
2020-10-03 20:51:30 -0400

    move user action to ts

---
+++ src/actions/user.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { getInstance } from "./index";
+import { History } from "history";
+import { IRootState } from "../reducers";
+import { Action } from "redux";
+import { ThunkAction } from "redux-thunk";
+import { User } from "../models";
+
+import { Constants } from "./types";
+
+const { FETCH_USER, UPDATE_USER, DELETE_USER } = Constants;
+export const fetchUser = (): ThunkAction<
+  void,
+  IRootState,
+  unknown,
+  Action<string>
+> => async (dispatch) => {
+  const response = await axios.get("/api/user");
+  dispatch({
+    type: FETCH_USER,
+    payload: response.data,
+  });
+};
+export const updateUser = (
+  values: User,
+  history: History
+): ThunkAction<void, IRootState, unknown, Action<string>> => async (
+  dispatch
+) => {
+  const instance = await getInstance();
+  const response = await instance.patch("/user", values);
+  history.push("/user");
+  dispatch({
+    type: UPDATE_USER,
+    payload: response.data,
+  });
+};
+
+export const deleteUser = (
+  history: History
+): ThunkAction<void, IRootState, unknown, Action<string>> => async (
+  dispatch
+) => {
+  const instance = await getInstance();
+  const response = await instance.delete("/user");
+  history.push("/");
+  dispatch({
+    type: DELETE_USER,
+    payload: response.data,
+  });
+};
