94320a8dfdae150cb02aa73e687c4ac2f1ed9d20
cpustejovsky <charles.pustejovsky@gmail.com>
2020-10-18 16:53:08 -0400

    move ProjectShow to tsx

--- src/components/projects/ProjectShow.tsx
+++ src/components/projects/ProjectShow.tsx
@@ -1,13 +1,38 @@
 import React, { useEffect } from "react";
-import { connect, useSelector } from "react-redux";
+import { connect, ConnectedProps } from "react-redux";
 import Project from "./Project";
-import Loader from "../partials/Loader.tsx";
+import Loader from "../partials/Loader";
 import { fetchProject, fetchProjectNotes } from "../../actions";
 import { Link as RouterLink } from "react-router-dom";
 import { Button } from "@material-ui/core";
-function ProjectShow({ fetchProject, fetchProjectNotes, match, history }) {
-  const project = useSelector((state) => state.projects[match.params.id]);
-  const notes = useSelector((state) => Object.values(state.notes));
+
+import { AppState } from "../../models/"
+
+//TODO: potential memory leak here because I'm unmounting and not cleaning up. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function
+//TODO: replace all the 'any' types
+
+const mapState = (state: AppState) => ({
+  projects: state.projects,
+  notes: state.notes,
+})
+
+const mapDispatch = {
+  fetchProject,
+  fetchProjectNotes
+}
+
+const connector = connect(mapState, mapDispatch)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+type Props = PropsFromRedux & {
+  history: any,
+  match: any
+}
+
+function ProjectShow(props: Props) {
+  const {projects, notes, fetchProjectNotes, fetchProject, history, match} = props;
+  const project = projects[match.params.id]
   useEffect(() => {
     fetchProject(match.params.id);
     fetchProjectNotes(match.params.id);
@@ -35,4 +60,4 @@ function ProjectShow({ fetchProject, fetchProjectNotes, match, history }) {
   }
 }
 
-export default connect(null, { fetchProject, fetchProjectNotes })(ProjectShow);
+export default connector(ProjectShow);
